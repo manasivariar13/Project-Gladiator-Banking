5b5f6857e2f09b53b92dcff631bf33b8fb58e97b
Manasi <manasi.variar@lntinfotech.com>
2022-08-01 21:12:05 +0530

    minor changes

--- src/app/fund-transfer/fund-transfer.component.ts
+++ src/app/fund-transfer/fund-transfer.component.ts
@@ -14,44 +14,49 @@ import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-fund-transfer',
   templateUrl: './fund-transfer.component.html',
-  styleUrls: ['./fund-transfer.component.css']
+  styleUrls: ['./fund-transfer.component.css'],
 })
 export class FundTransferComponent implements OnInit {
   statusMessage: string;
-  statusCode : string;
+  statusCode: string;
   transaction: FundTransferDto = new FundTransferDto();
 
   accountSummaryStatus: AccountSummaryStatus = new AccountSummaryStatus();
   beneficiaries: BeneficiaryDetails[];
-  constructor(private service:ViewBeneficiaryService , private transactionService:TransactionService  ,
-     private router : Router, private accountService: AccountService,private otpService:GenerateOtpService ,
-     private bnIdle: BnNgIdleService ,private SpinnerService: NgxSpinnerService) {
-      this.bnIdle.startWatching(300).subscribe((res) => {
-        if(res) {
-          this.router.navigate(['session-expired']);
-        }
-      })
-      
-    };
+  constructor(
+    private service: ViewBeneficiaryService,
+    private transactionService: TransactionService,
+    private router: Router,
+    private accountService: AccountService,
+    private otpService: GenerateOtpService,
+    private bnIdle: BnNgIdleService,
+    private SpinnerService: NgxSpinnerService
+  ) {
+    this.bnIdle.startWatching(300).subscribe((res) => {
+      if (res) {
+        this.router.navigate(['session-expired']);
+      }
+    });
+  }
   message: string;
   error: boolean;
   transactionDetails: string;
-  custId:number;
-  otp:string;
-
-
+  custId: number;
+  otp: string;
 
   ngOnInit(): void {
-    this.custId=parseInt(sessionStorage.getItem('custId'));
+    this.custId = parseInt(sessionStorage.getItem('custId'));
     this.showBeneficiary();
 
-    this.accountSummaryStatus.accountNumber=parseInt(sessionStorage.getItem('accountNumber'));
-    this.transaction.fromAccount=sessionStorage.getItem('accountNumber');
+    this.accountSummaryStatus.accountNumber = parseInt(
+      sessionStorage.getItem('accountNumber')
+    );
+    this.transaction.fromAccount = sessionStorage.getItem('accountNumber');
 
     // this.accountService.showAccountSummary(this.custId).subscribe(response => {
     //   if (response.statusCode === "SUCCESS") {
     //     this.accountSummaryStatus.accountNumber = response.accountNumber;
-        
+
     //     sessionStorage.setItem("accountNumber",String(response.accountNumber));
     //     this.transaction.fromAccountNumber=parseInt(sessionStorage.getItem('accountNumber'));
     //   }
@@ -60,97 +65,82 @@ export class FundTransferComponent implements OnInit {
     //     this.message = response.statusMessage;
     //   }
     // })
-    
   }
 
   getOTP() {
-    this.transactionService.getOTP(this.custId).subscribe(data => {
-      if (data.statusCode === "SUCCESS") {
+    this.transactionService.getOTP(this.custId).subscribe((data) => {
+      if (data.statusCode === 'SUCCESS') {
         // this.statusMessage = data.statusMessage;
         // this.statusCode = data.statusCode;
         // document.getElementById("openModalButton").click();
         // this.onSubmit();
-      }
-      else {
+      } else {
         this.statusMessage = data.statusMessage;
         this.statusCode = data.statusCode;
-        document.getElementById("openModalButton").click();
+        document.getElementById('openModalButton').click();
       }
-    }
-    )
+    });
   }
 
-
-  onSubmit(){
+  onSubmit() {
     // this.transaction.fromAccountNumber=parseInt(sessionStorage.getItem('accountNumber'));
     this.SpinnerService.show();
     console.log(this.transaction);
-    // if(this.transaction.otp==this.otp){
-
-
-    this.transactionService.transfer(this.transaction).subscribe( data =>{
-      if(data.statusCode === "SUCCESS"){
-        this.SpinnerService.hide();
-        // this.transactionDetails=data.transactionDetail;
-        alert(data.statusMessage);
-        // sessionStorage.setItem('transactionId',String(this.transactionDetails.id));
-        // sessionStorage.setItem('fromAccountNumber',String(this.transactionDetails.fromAccountNumber));
-        // sessionStorage.setItem('toAccountNumber',String(this.transactionDetails.toAccountNumber));
-        // // sessionStorage.setItem('remarks',this.transactionDetails.remarks);
-        // sessionStorage.setItem('amount',String(this.transactionDetails.amount));
-        // sessionStorage.setItem('transactionAmount',String(this.transactionDetails.amount));
-        // sessionStorage.setItem('transactionType',this.transactionDetails.transactionType);
-        // sessionStorage.setItem('transactionStatus',data.statusCode);
-        this.router.navigate(['account-summary']);
-        
-        
-      }
-      else{
-        this.SpinnerService.hide();
-        sessionStorage.setItem('transactionStatus',data.statusCode);
-        alert(data.statusMessage);
-      }
-    })
-  // } 
-    
-    // else {
-    //   this.SpinnerService.hide();
-    //   alert("OTP is incorrect");
-    // }
+    if (this.transaction.otp == this.otp) {
+      this.transactionService.transfer(this.transaction).subscribe((data) => {
+        if (data.statusCode === 'SUCCESS') {
+          this.SpinnerService.hide();
+          // this.transactionDetails=data.transactionDetail;
+          alert(data.statusMessage);
+          // sessionStorage.setItem('transactionId',String(this.transactionDetails.id));
+          // sessionStorage.setItem('fromAccountNumber',String(this.transactionDetails.fromAccountNumber));
+          // sessionStorage.setItem('toAccountNumber',String(this.transactionDetails.toAccountNumber));
+          // // sessionStorage.setItem('remarks',this.transactionDetails.remarks);
+          // sessionStorage.setItem('amount',String(this.transactionDetails.amount));
+          // sessionStorage.setItem('transactionAmount',String(this.transactionDetails.amount));
+          // sessionStorage.setItem('transactionType',this.transactionDetails.transactionType);
+          // sessionStorage.setItem('transactionStatus',data.statusCode);
+          this.router.navigate(['account-summary']);
+        } else {
+          this.SpinnerService.hide();
+          sessionStorage.setItem('transactionStatus', data.statusCode);
+          alert(data.statusMessage);
+        }
+      });
+    } else {
+      this.SpinnerService.hide();
+      alert('OTP is incorrect');
+    }
   }
-  showBeneficiary(){
-    this.service.showBeneficiary(parseInt(sessionStorage.getItem('accountNumber'))).subscribe(data =>{
-      if(data.statusCode==="SUCCESS"){
-        this.beneficiaries=data.beneficiaryDto;
-      }
-    })
+  showBeneficiary() {
+    this.service
+      .showBeneficiary(parseInt(sessionStorage.getItem('accountNumber')))
+      .subscribe((data) => {
+        if (data.statusCode === 'SUCCESS') {
+          this.beneficiaries = data.beneficiaryDto;
+        }
+      });
   }
 
-  onClick($event:any){
+  onClick($event: any) {
     $event.preventDefault();
     this.SpinnerService.show();
-    this.transactionService.getOTP(this.custId).subscribe(data=>{
-      if(data.statusCode=="SUCCESS"){
+    this.transactionService.getOTP(this.custId).subscribe((data) => {
+      if (data.statusCode == 'SUCCESS') {
         this.SpinnerService.hide();
         this.otp = data.statusMessage;
-        this.statusMessage="OTP has been sent to your registered Email ID";
-        document.getElementById("openModalButton").click();
-      }
-      else{
+        this.statusMessage = 'OTP has been sent to your registered Email ID';
+        document.getElementById('openModalButton').click();
+      } else {
         this.SpinnerService.hide();
-        this.statusMessage="OTP Generation Failed!!Click to try again";
-        document.getElementById("openModalButton").click();
+        this.statusMessage = 'OTP Generation Failed!!Click to try again';
+        document.getElementById('openModalButton').click();
       }
-    })
+    });
   }
 
-  
-  
-
-  goToAddBeneficiary($event:any){
+  goToAddBeneficiary($event: any) {
     $event.preventDefault();
     this.router.navigate(['add-beneficiary']);
   }
-
-
 }
--- src/app/models/register-ib.ts
+++ src/app/models/register-ib.ts
@@ -2,4 +2,5 @@ export class RegisterIb{
     public accountNumber : number;
     public loginPassword: string;
     public transactionPassword: string;
+    public failedAttempt: number = 0;
 }
